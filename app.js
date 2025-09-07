4e49b33ced3136e5f06dd631c0af75185a465e70
irfan-official <irfanofficial.work@gmail.com>
2025-09-07 06:27:25 +0600

    added origin: true

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "*", // ✅ Vercel will auto-handle frontend origin
+    origin: true, // ✅ Vercel will auto-handle frontend origin
     credentials: true,
   })
 );
